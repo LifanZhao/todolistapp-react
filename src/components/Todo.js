b20ecf2353789748cd90a79723c652a2a8aec80e
Lifan Zhao <charlenezhaolf@gmail.com>
2025-08-26 23:12:26 +1000

    update todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { fontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export const Todo = () => {
   return (
     <div className='Todo'>
         <p>Go to School</p>
         <div>
-            
+            <fontAwesomeIcon icon={faPenToSquare} />
+            <fontAwesomeIcon icon={faTrash} />
         </div>
     </div>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'; //version4
+import { Todo } from './Todo';
 uuidv4();
 
 
@@ -22,6 +23,10 @@ export const TodoWrapper = () => {
   return (
     <div className='TodoWrapper'>
         <TodoForm addTodo = {addTodo}></TodoForm>
+        {/* {todos.map((todo, index) => (
+        16'58
+        ))} */}
+        <Todo />
     </div>
   )
 }
