2101f9104e9f376ee6bb4058c9808e6ddb10a907
Lifan Zhao <charlenezhaolf@gmail.com>
2025-09-02 23:12:31 +1000

    update delete and complete function

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -6,6 +6,7 @@ uuidv4();
 
 
 export const TodoWrapper = () => {
+    //hook 自动生成setTodos函数用于更新状态
     const [todos, setTodos] = useState([])
 
     const addTodo = todo => {
@@ -20,12 +21,28 @@ export const TodoWrapper = () => {
         ])
         console.log(todos)
     }
+
+    // complete function to change the appearance
+    const toggleComplete = id => {
+      setTodos(todos.map(todo => todo.id === id ? {
+        ...todo, completed: !todo.completed} : todo
+      ))
+    }
+
+    // delete function to filter all the ids not meet the criteria
+    const deleteTodo = id => {
+      setTodos(todos.filter(todo => todo.id !== id))
+    }
+    //23'28s
+
   return (
     <div className='TodoWrapper'>
       <h1> Get things Done!</h1>
         <TodoForm addTodo={addTodo} />
-        {todos.map((todo, index) => (
-          <Todo task={todo} key={index} />
+        {todos.map((todo) => (
+          <Todo task={todo} key={todo.id} 
+          toggleComplete={toggleComplete}
+          deleteTodo = {deleteTodo}/>
         ))}
         
     </div>
